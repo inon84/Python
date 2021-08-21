83982203734678131b4f9ede5e9bd4de165860f2
Inon Moshkowitz <inon84@gmail.com>
2021-08-21 03:02:40 +0300

    .

--- WeatherApp/app.py
+++ WeatherApp/app.py
@@ -1,11 +1,11 @@
 from datetime import datetime as dt
 from datetime import timezone, timedelta, tzinfo
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-#TODO: check this after fixing the client "ip_address" fetch to fetch client "city"
+#TODO: check this after fixing the client "ip_address" to fetch client "city"
 # def get_geo(ip_address):
 #     try:
 #         response = requests.get("http://api.ipstack.com/{}?access_key=053d8fb4ec21e556609a665c5006937a".format(ip_address))
@@ -17,19 +17,25 @@ app = Flask(__name__)
 #         return "Unknown"
 
 @app.route('/')
-@app.route('/<city>')
+@app.route('/search', methods=['POST', 'GET'])
 def index(city=None):
-    if city != None:
-        #TODO: Handle errors on city name, if not 200 OK
-        city=city
-    else: city = "Tel Aviv"
+    if request.method == 'GET':
+        city = request.args.get('city')
+        if city == None:
+            city = "Tel Aviv"
+    # if request.method == 'POST':
+    #     if city != None:
+    #         city = city
+    #     else:
+    #         city = 'Tel Aviv'
+    
     #TODO: fix this to get the client ip_address to fetch city
     # ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     # city = get_geo(ip_address)
     
-    # FORECAST_URL(full) https://api.openweathermap.org/data/2.5/weather?q=Las%20Vegas&appid=ebb0bc77fb0c2a744c9b44d4c97b2631&units=metric
+    # FORECAST_URL (FULL URL for mocks) https://api.openweathermap.org/data/2.5/weather?q=Las%20Vegas&appid=ebb0bc77fb0c2a744c9b44d4c97b2631&units=metric
     
-    API_KEY = 'ebb0bc77fb0c2a744c9b44d4c97b2631' # TODO: Hide in env vars?
+    API_KEY = 'ebb0bc77fb0c2a744c9b44d4c97b2631' # TODO: Hide in ENV Vars
     WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'
     FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&units=metric'
 
@@ -45,8 +51,11 @@ def index(city=None):
     r = requests.get(WEATHER_URL.format(city, API_KEY)).json()
     forecast = requests.get(FORECAST_URL.format(city, API_KEY)).json()
     
-    rweather = r['weather'][0]
-    rsys = r['sys']
+    if r['cod'] == 200:
+        rweather = r['weather'][0]
+        rsys = r['sys']
+    else:
+        pass
         
     #tz offset string
     offset = int(dt.utcfromtimestamp(r['timezone']).strftime('%H'))
@@ -67,8 +76,8 @@ def index(city=None):
         'temperature': round(r['main']['temp']),
         'icon': rweather['icon'],
         # 'sunrise': dt.utcfromtimestamp(rsys['sunrise'] + offset).strftime('%H:%m'),
-        'sunrise': dt.utcfromtimestamp((rsys['sunrise']) - offset).strftime('%H:%m'),
-        'sunset': dt.utcfromtimestamp(rsys['sunset'] - offset).strftime('%H:%m'),
+        'sunrise': dt.utcfromtimestamp((rsys['sunrise']) + offset).strftime('%H:%m'),
+        'sunset': dt.utcfromtimestamp(rsys['sunset'] + offset).strftime('%H:%m'),
         'timezone': str(tz_show)
     }
     
@@ -81,6 +90,7 @@ def index(city=None):
     }
     
     return render_template('index.html', 
+                            city=city,
                             now_local=now_local, 
                             now_utc=now_utc, 
                             now=now, 
