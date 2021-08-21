a2ee06204a4603cf5b627eaeca49eecbdfdace3f
Inon Moshkowitz <inon84@gmail.com>
2021-08-21 22:59:23 +0300

    .

--- Scytale/fast.py
+++ Scytale/fast.py
@@ -1,18 +1,28 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
+from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount('/static', StaticFiles(directory='static/'), name='static')
+templates = Jinja2Templates(directory='templates/')
 
 
-@app.get("/pr", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id}
+@app.get('/pr', response_class=HTMLResponse)
+async def read_items(request: Request):
+    # return "GET."
+    result = 'TEST'
+    # return templates.TemplateResponse('index.html', 'request': request)
+    return templates.TemplateResponse('index.html', context={'request': request})
+
+@app.post('/pr', response_class=HTMLResponse)
+async def search(context):
+    return 'POST.', context
+    # return templates.TemplateResponse('index.html')
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run('fast:app', host='localhost', port=8000, reload=True)
