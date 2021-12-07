779dbf121db8fc2a04f53b0adca3240ee7af5caa
Inon Moshkowitz <inon84@gmail.com>
2021-12-07 18:26:47 +0200

    .

--- IMDbAlt/imdbalt.py
+++ IMDbAlt/imdbalt.py
@@ -1,19 +1,15 @@
 import requests
 
 POPULAR_MOVIES_URL = "https://imdb8.p.rapidapi.com/title/get-most-popular-movies"
-
 querystring = {"homeCountry":"US","purchaseCountry":"US","currentCountry":"US"}
-
 HEADERS = {
     'x-rapidapi-host': "imdb8.p.rapidapi.com",
     'x-rapidapi-key': "321f72bce3msh7161ef254383001p1b98b3jsn3d9b8c33523d"
     }
-
 response = requests.request("GET", POPULAR_MOVIES_URL, headers=HEADERS, params=querystring).json()
 # print(response.text)
 
 popular_movies = []
-
 def clean_movies_list():
     for movie in response:
         movie = movie.split('/')[2]
